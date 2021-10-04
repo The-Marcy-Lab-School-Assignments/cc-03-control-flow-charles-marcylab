ed1baa15adbbae69c52f992e614d58a9cfe97f32
charles yarney <charlesyarney@gmail.com>
2021-10-04 14:30:38 +0000

    Didnt complete bonus challenge

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 //1
-function countToTen(){
-  
+function countFromOne(num){
+    for(let i = 1; i <= num; i++){
+      console.log(i)
 }
+//countFromOne()
 
 //2
-function countFromOne(){
-  
+function countEveryOdd(){
+    for(let i = 1; i < num; i++){
+        if(1 % 2 === 0){
+            console.log(i)
+        }
+    }
 }
+//countEveryOdd()
 
 //3
-function countEveryEven(){
-  
+function isNegative(){
+  if (num < 0){
+    return 'true';
+  }else{ 
+    return 'false';
+  }
 }
 
+//console.log(isNegative())
 //4
-function countEveryOdd(){
-  
+function betweenFiveAndTwenty(num){
+    if(num >= 5 && num <= 20){
+        return true;
+    }else{
+       return false;
+    }
 }
-
-//5 
-function countEvens(){
+console.log(betweenFiveAndTwenty(65))
   
-}
+//5
+function isAllLowerCase(str){
+    const lowercase = str.toLowerCase()
+    if(lowercase === str){
+        return true
+    }else{
+        return false
+    }
+} 
 
+//console.log(isAllLowerCase('hey'))
+
+//Bonus(6)
